1bed43c0106aaf51afce12ce8e51a2507178f336
Hiroyasu Akada <hakada@slurm-submit-0.mpi-inf.mpg.de>
2022-11-09 23:48:25 +0100

    add image visualizer

--- visualize_image.py
+++ visualize_image.py
@@ -5,36 +5,6 @@ from utils.util import tensor2im, save_image
 import numpy as np
 import torch
 
-# # convert a tensor into a numpy array
-# def tensor2im(image_tensor, bytes=255.0, imtype=np.uint8, is_depth=False, is_heatmap=False):
-#     if image_tensor.dim() == 3:  # (C, H, W)
-#         image_tensor = image_tensor.cpu().float()
-#     else:
-#         image_tensor = image_tensor[0].cpu().float()
-
-#     if is_depth:
-#         image_tensor = image_tensor * bytes
-#     elif is_heatmap:
-#         image_tensor = torch.clamp(torch.sum(image_tensor, dim=0, keepdim=True), min=0.0, max=1.0) * bytes
-#     else:
-#         # image_tensor = (image_tensor + 1.0) / 2.0 * bytes
-#         image_tensor = denormalize_ImageNet(image_tensor) * bytes
-
-#     image_numpy = (image_tensor.permute(1, 2, 0)).numpy().astype(imtype)
-#     return image_numpy
-
-# def denormalize_ImageNet(x):
-#     mean = torch.Tensor([0.485, 0.456, 0.406]).view(3, 1, 1)
-#     std = torch.Tensor([0.229, 0.224, 0.225]).view(3, 1, 1)
-#     return x * std + mean
-
-# def save_image(image_numpy, image_path):
-#     if image_numpy.shape[2] == 1:
-#         image_numpy = image_numpy.reshape(image_numpy.shape[0], image_numpy.shape[1])
-#     image_numpy = Image.fromarray(image_numpy)
-#     image_numpy.save(image_path)
-#     # imageio.imwrite(image_path, image_numpy)
-
 
 if __name__ == '__main__':
 
